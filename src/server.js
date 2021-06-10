361fdd705a74c7ed03a93699fae97198de931492
Pietro Licenza <pietro.licenza@intelligenti.com.br>
2021-06-09 21:49:49 -0300

    inicializando servidor

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+// const routes = require('./routes');
+require('dotenv').config();
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+// server.use(routes);
+
+server.listen(3001, () => console.log('Rodando...'));
